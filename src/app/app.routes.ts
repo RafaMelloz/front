e2dffa02a035c0b91d182de44c955dd66127bb8f
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2025-08-12 19:08:26 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,6 @@ import { authGuard } from './guards/auth.guard';
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
     { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [authGuard] },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // redireciona raiz para dashboard
+    { path: '**', redirectTo: 'dashboard' } // fallback
 ];
